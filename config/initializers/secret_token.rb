6017bc79fee8594fd93225bdfb81bdac586e703e
tsuchikazu <huneyhunt77@gmail.com>
2012-07-07 15:52:33 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shushu::Application.config.secret_token = '5d75e927f2063926ce71383527355c25158c5bbdbaaad3c89e804ae47ef51e4c1c2d50f13804db9eb074e2890774ca5945da97630d0640026425bbc7eff0ae71'
