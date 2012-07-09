69c0a4adea47d52bfbdfc27488b84dda959f926f
tsuchikazu <huneyhunt77@gmail.com>
2012-07-10 00:56:03 +0900

    色々微修正

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -4,13 +4,21 @@ class PartsController < ApplicationController
     @condition = params[:condition] ||= {}
     @googles = {}
     @youtubes = {}
-    render unless @condition[:query] 
+
+    render unless @condition[:search] 
 
 
+    @page = params[:page]
+    per_page = 9 
     if @condition[:target] == 'youtube'
-      @youtubes = YoutubeSearch.search(@condition[:query], page: params[:page], per_page:9) 
+      # 1から始まり2から指定可能
+      @youtubes = YoutubeSearch.search(@condition[:search], page: @page, per_page: per_page) 
     else
-      @googles = GoogleAjax::Search.images(@condition[:query])[:results]
+      per_page = 6
+      # 0から始まりper_pageを掛ける
+      @page = 1 if @page == nil
+      start = (@page.to_i - 1) * per_page
+      @googles = GoogleAjax::Search.images(@condition[:search], {start: start,  rsz: per_page})[:results]
     end
   end
 end
